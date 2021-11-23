3fe781475751ba1af7e0f27078bd00879648ebd8
Matt Anderson <m.anderson@epyx.co.uk>
2021-11-23 13:45:05 +0000

    AppPool update

--- OOup/Tasks/InstallIISAppPool.cs
+++ OOup/Tasks/InstallIISAppPool.cs
@@ -8,8 +8,8 @@ namespace OOup.Tasks
 {
     public class InstallIISAppPool : RunPowershell
     {
-        const string inlineScript = "Param([string] $AppPoolName)if (!(Test-Path IIS:\\AppPools\\$AppPoolName -pathType container)){Write-Output \"Create the app pool\" ; $appPool = New-WebAppPool -Name $AppPoolName -Force}}";
-        public InstallIISAppPool(string appPool, string directory) : base(inlineScript, appPool, directory)
+        const string inlineScript = "Param([string] $AppPoolName)if (!(Test-Path IIS:\\AppPools\\$AppPoolName -pathType container)){ Write-Output \"Create the app pool\" ; $appPool = New-WebAppPool -Name $AppPoolName -Force;}";
+        public InstallIISAppPool(string appPool) : base(inlineScript, appPool)
         {
         }
     }
