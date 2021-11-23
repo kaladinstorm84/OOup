aaa5e4cdd12b2a77bd2b689b23eb051f8c65d89d
Matt Anderson <m.anderson@epyx.co.uk>
2021-11-23 13:37:45 +0000

    InstallIISAppPool

---
+++ OOup/Tasks/InstallIISAppPool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOup.Tasks
+{
+    public class InstallIISAppPool : RunPowershell
+    {
+        const string inlineScript = "Param([string] $AppPoolName)if (!(Test-Path IIS:\\AppPools\\$AppPoolName -pathType container)){Write-Output \"Create the app pool\" ; $appPool = New-WebAppPool -Name $AppPoolName -Force}}";
+        public InstallIISAppPool(string appPool, string directory) : base(inlineScript, appPool, directory)
+        {
+        }
+    }
+}
